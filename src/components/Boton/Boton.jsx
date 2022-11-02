85cf5ca4e6b4dfd041571be3edc1cd7a8313c810
Juan Arenas <juanandrenas@gmail.com>
2022-11-02 11:33:01 -0500

    version1

---
+++ src/components/Boton/Boton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Boton({
+
+}) {
+  return (
+    <button class="h-8 w-14 bg-indigo-500 border rounded-lg mr-5 font-bold text-blue-50">
+        Salir
+    </button>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import Boton from './Boton/Boton'
+
+
+function Navbar() {
+
+  return (
+    <nav class="p-3 bg-cyan-50">
+      <div class="container flex flex-wrap justify-between items-center mx-auto">
+        <a href="#" class="flex items-center">
+          <img src="https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png" class="h-10 mt-2 mb-2 mr-5" />
+          <span class="self-center font-bold">Buen día</span>
+        </a>
+        <button id="triggerEl" data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500" aria-controls="navbar-solid-bg" aria-expanded="false">
+          <span class="sr-only">Abrir menú</span>
+          <svg id="triggerEl" class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        </button>
+        <div class="hidden self-center flex justify-end md:block md:w-auto" id="triggerEl">
+          <ul class="flex flex-col mt-4 bg-gray-50 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+            <li>
+              <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Inicio</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Título 1</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Título 2</a>
+            </li>
+            <li>
+              <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Título 3</a>
+            </li>
+          </ul>
+        </div>
+        <Boton />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
